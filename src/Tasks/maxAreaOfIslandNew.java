bff0d91e7dbed89ddb54d30ab722f2b6a4a91f19
luftwaffe1985 <putik1985@yandex.ru>
2023-01-06 18:13:11 +0300

    Initial commit

---
+++ src/Tasks/maxAreaOfIslandNew.java
@@ -0,0 +1,44 @@
+package Tasks;
+
+import java.util.*;
+
+public class maxAreaOfIslandNew {
+    int[][] STEPS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    max = Math.max(max, dfs(i, j, grid));
+                }
+            }
+        }
+        return max;
+    }
+
+    public int dfs(int i, int j, int[][] grid) {
+
+        int count = 0;
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[] { i, j });
+        grid[i][j] = 0;
+        while (!stack.isEmpty()) {
+            int[] cur = stack.pop();
+            int curI = cur[0];
+            int curJ = cur[1];
+
+            count++;
+
+            for (int[] step : STEPS) {
+                int newI = curI + step[0];
+                int newJ = curJ + step[1];
+                if (newI >= 0 && newI < grid.length && newJ >= 0 && newJ < grid[0].length && grid[newI][newJ] == 1) {
+                    grid[newI][newJ] = 0;
+                    stack.push(new int[] { newI, newJ });
+                }
+            }
+        }
+        return count;
+    }
+}
